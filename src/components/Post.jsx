f75fdd7c762ff97e7ca21eedbbb56edc268ff235
HarshaFdo <fernando.harsha2016@gmail.com>
2025-02-27 08:14:26 +0530

    app.jsx 1st changes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,9 @@
-const names = ['Harsha', 'Ashan'] 
-
 function Post() {
-  const choesenName = Math.random() > 0.5 ? names[0] : names[1];
-
+  
   return (
     <div>
-      <p>{choesenName}</p>
-      <p>React.js is awesome!</p>
+      <p>{}</p>
+      <p>{}</p>
     </div>
   );
 }
