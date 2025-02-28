5ee271848987aa29cb8dcbc113cf48c7dd228541
HarshaFdo <fernando.harsha2016@gmail.com>
2025-02-28 22:42:49 +0530

    form submission

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
-function NewPost (props) {
+function NewPost ({ onCancel }) {
+  const [enterBody, setEnterBody] = useState('');
+  const [enterAuthor, setEnterAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnterBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnterAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: enterBody,
+      author: enterAuthor
+    };
+    console.log(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange}/>
+        <input type="text" id="name" required onChange={authorChangeHandler}/>
+      </p>
+      <p className={classes.actions}>
+        <button type='button' onClick={onCancel} >Cancel</button>
+        <button>Submit</button>
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,42 +6,17 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({isPosting, onStopPosting}) {
-  const [enterBody, setEnterBody] = useState('');
-  const [enterAuthor, setEnterAuthor] = useState('');
-
-  function bodyChangeHandler(event) {
-    setEnterBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnterAuthor(event.target.value);
-  }
-
-  //let modalContent;
-
-  //if (modalIsVisible) {
-    //modalContent = <Modal onClose={hideModalHandler}>
-    //<NewPost 
-      //onBodyChange = {bodyChangeHandler} 
-      //onAuthorChange = {authorChangeHandler} 
-    ///>
-  //</Modal>
-  //}
-
   return (
     <>
       {isPosting && (
         <Modal onclose={onStopPosting}>
-          <NewPost 
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange = {authorChangeHandler}
+          <NewPost onCancel={onStopPosting}
           >
           </NewPost>
 
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enterAuthor} body={enterBody}/>
         <Post author="Ashan" body="Check out the full course!" />
       </ul>
     </>
