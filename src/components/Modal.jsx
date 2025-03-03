f787d3c55ca720160a1aec34e300a973f7e34a28
HarshaFdo <fernando.harsha2016@gmail.com>
2025-03-03 10:17:12 +0530

    change the new post css file

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,17 @@
-import { Children } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import classes from './Modal.module.css';
 
-function Modal({ children, onclose }) {
+function Modal({ children }) {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    navigate('..');
+  }
+
   return (
     <>
-      <div className={classes.backdrop} onClick={onclose}/>
+      <div className={classes.backdrop} onClick={closeHandler}/>
       <dialog open className={classes.modal}> 
         {children}
       </dialog>
